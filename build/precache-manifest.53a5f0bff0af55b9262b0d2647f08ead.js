c54ff4b1f05e9f515d85c61f1013977a23c974a8
Avarayr <mikamika83@gmail.com>
2020-11-23 20:58:36 -0800

    added .env to gitignore

---
+++ build/precache-manifest.53a5f0bff0af55b9262b0d2647f08ead.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b220f67ae8f75fddf535689a59787d5e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "16c3116cb52df27cf869",
+    "url": "./static/css/main.a2b7b0b6.css"
+  },
+  {
+    "revision": "b3badc05f52f8e8cd580",
+    "url": "./static/js/1.b9528361.chunk.js"
+  },
+  {
+    "revision": "19aedc330a0e6f3cd4ac3fb01654833b",
+    "url": "./static/js/1.b9528361.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16c3116cb52df27cf869",
+    "url": "./static/js/main.74619b6b.js"
+  },
+  {
+    "revision": "9c58c99a8df009c2dd446c527172a001",
+    "url": "./static/js/main.74619b6b.js.LICENSE.txt"
+  }
+]);
